8112e910729877e177904cd680bddfb25270b7d8
Blake Edwards <bedwards28@users.noreply.github.com>
2020-07-05 08:42:06 -0500

    Recommend best match

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word):
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
     else:
         return "That word does not exist.  Please double check it."
 
-word = input("Enter word: ")
+word = input("Enter word: ").lower()
 
 print(translate(word))
