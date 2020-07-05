cdd8d41ff372501bffaa7bad738b02262cec4b78
Blake Edwards <bedwards28@users.noreply.github.com>
2020-07-05 08:57:56 -0500

    Confirm user input

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ def translate(word):
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
-        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+        user_input = input("Did you mean %s instead? Enter y(es) or n(o): " % get_close_matches(word, data.keys())[0])
+        if user_input.lower() == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif user_input.lower() == "n":
+            return "That word doesn't exist.  Try again."
+        else:
+            return "What didn't you understand about entering yes or no?"
     else:
         return "That word does not exist.  Please double check it."
 
